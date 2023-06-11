93296055c39d0d8db9f0ff72ccd50279db2ad01a
leo10torres <leo.torres@mi.unc.edu.ar>
2023-06-11 20:33:27 -0300

    Se agrego un caso en el calculo de t que no se estaba teniendo en cuenta

--- src/chi_square_test.py
+++ src/chi_square_test.py
@@ -7,39 +7,38 @@ import matplotlib.pyplot as plt
 
 def p_value_gamma(data):
     # frequency, and interval edges
-    N, bins, _ = plt.hist(parse_sample("../sample23.dat"), bins="auto")
+    N, bins, _ = plt.hist(parse_sample("../sample23.dat"), bins=9)
     k = len(bins) - 1
     n = len(data)
     a, theta = estimate_gamma(data)
 
-    t = 0
-    for i in range(k-1):
+    t = gamma.cdf(bins[0], a, scale=theta)
+    for i in range(k):
         F_a = gamma.cdf(bins[i], a, scale=theta)
         F_b = gamma.cdf(bins[i+1], a, scale=theta)
         p_i = F_b - F_a
         t += ((N[i] - n*p_i))**2 / (n*p_i)
-    # print(t)
-    N[k-1] += 1
+    t += 1 - gamma.cdf(bins[k], a, scale=theta)
+
     return 1 - chi2.cdf(t,k-1)
 
 def p_value_lognormal(data):
     # frequency, and interval edges
-    N, bins, _ = plt.hist(parse_sample("../sample23.dat"), bins="auto")
+    N, bins, _ = plt.hist(parse_sample("../sample23.dat"), bins=9)
     k = len(bins) - 1
     n = len(data)
     mu, sigma = estimate_log_normal(data)
 
-    t = 0
+    t = lognorm.cdf(bins[0], s=sigma, scale=exp(mu))
     for i in range(k):
         F_a = lognorm.cdf(bins[i], s=sigma, scale=exp(mu))
         F_b = lognorm.cdf(bins[i+1], s=sigma, scale=exp(mu))
         p_i = F_b - F_a
         t += ((N[i] - n*p_i))**2 / (n*p_i)
-    # print(t)
+    t += lognorm.cdf(bins[k], s=sigma, scale=exp(mu))
 
-    N[k-1] += 1
     return 1 - chi2.cdf(t,k-1)
 
 print (p_value_lognormal(parse_sample("../sample23.dat")))
 print (p_value_gamma(parse_sample("../sample23.dat")))
-
+print (p_value_gamma(parse_sample("../sample23.dat")) -p_value_lognormal(parse_sample("../sample23.dat")) )
