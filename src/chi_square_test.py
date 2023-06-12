e72e2addb76a3303ffc0769e0630a4531385bf41
leo10torres <leo.torres@mi.unc.edu.ar>
2023-06-12 14:55:22 -0300

    caso borde de los intervalos arreglados

--- src/chi_square_test.py
+++ src/chi_square_test.py
@@ -3,42 +3,44 @@ from parse_sample import parse_sample
 from scipy.stats import lognorm, gamma, chi2
 from estimations import estimate_log_normal, estimate_gamma
 import matplotlib.pyplot as plt
-
+from math import sqrt
 
 def p_value_gamma(data):
     # frequency, and interval edges
-    N, bins, _ = plt.hist(parse_sample("../sample23.dat"), bins=9)
+    N, bins, _ = plt.hist(parse_sample("../sample23.dat"), bins=16)
     k = len(bins) - 1
     n = len(data)
     a, theta = estimate_gamma(data)
 
-    t = gamma.cdf(bins[0], a, scale=theta)
-    for i in range(k):
+    p_i = gamma.cdf(bins[1], a, scale=theta)
+    t = ((N[0] - n*p_i))**2 / (n*p_i)
+    for i in range(1, k):
         F_a = gamma.cdf(bins[i], a, scale=theta)
         F_b = gamma.cdf(bins[i+1], a, scale=theta)
         p_i = F_b - F_a
         t += ((N[i] - n*p_i))**2 / (n*p_i)
-    t += 1 - gamma.cdf(bins[k], a, scale=theta)
-
+    p_i = 1 - gamma.cdf(bins[k-1], a, scale=theta)
+    t += ((N[k-1] - n*p_i))**2 / (n*p_i)
     return 1 - chi2.cdf(t,k-1)
 
 def p_value_lognormal(data):
     # frequency, and interval edges
-    N, bins, _ = plt.hist(parse_sample("../sample23.dat"), bins=9)
+    N, bins, _ = plt.hist(parse_sample("../sample23.dat"), bins=16)
     k = len(bins) - 1
     n = len(data)
     mu, sigma = estimate_log_normal(data)
 
-    t = lognorm.cdf(bins[0], s=sigma, scale=exp(mu))
-    for i in range(k):
+    p_i = lognorm.cdf(bins[1], s=sigma, scale=exp(mu))
+    t = ((N[0] - n*p_i))**2 / (n*p_i)
+    for i in range(1,k):
         F_a = lognorm.cdf(bins[i], s=sigma, scale=exp(mu))
         F_b = lognorm.cdf(bins[i+1], s=sigma, scale=exp(mu))
         p_i = F_b - F_a
         t += ((N[i] - n*p_i))**2 / (n*p_i)
-    t += lognorm.cdf(bins[k], s=sigma, scale=exp(mu))
+    p_i = 1 - lognorm.cdf(bins[k-1], s=sigma, scale=exp(mu))
+    t += ((N[k-1] - n*p_i))**2 / (n*p_i)
 
     return 1 - chi2.cdf(t,k-1)
 
-print (p_value_lognormal(parse_sample("../sample23.dat")))
-print (p_value_gamma(parse_sample("../sample23.dat")))
-print (p_value_gamma(parse_sample("../sample23.dat")) -p_value_lognormal(parse_sample("../sample23.dat")) )
+print ("lognormal: ", p_value_lognormal(parse_sample("../sample23.dat")))
+print ("gamma: ", p_value_gamma(parse_sample("../sample23.dat")))
